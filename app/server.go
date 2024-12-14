3ef59b26423883158ce0775975e1694dcd17287c
anti-duhring <mateus@alternativepayments.io>
2024-12-14 18:35:57 -0300

    created handleConnection func

--- app/server.go
+++ app/server.go
@@ -1,22 +1,44 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
 )
 
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	header := []byte{}
+	var msgSize uint32 = 0
+	msgSizeBytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(msgSizeBytes, msgSize)
+
+	var cID uint32 = 7
+	cIDbytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(cIDbytes, cID)
+
+	header = append(header, msgSizeBytes...)
+	header = append(header, cIDbytes...)
+
+	conn.Write(header)
+}
+
 func main() {
-	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	defer l.Close()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	handleConnection(conn)
 }
